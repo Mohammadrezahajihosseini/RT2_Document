379df2d3921a84abb20319880e291b8bf5aa2e3b
Mohammad Reza Haji Hosseini <80394968+Mohammadrezahajihosseini@users.noreply.github.com>
2022-04-23 13:45:10 +0200

    Update master.py

--- scripts/master.py
+++ scripts/master.py
@@ -1,85 +1,71 @@
 #!/usr/bin/env python3
-"""
-.. module:: master
-
-:platform: Unix
-:synopsis: Python module for control behavior of robot
-
-:moduleauthor: Mohammad Reza Haji Hosseini <4394567@studenti.unige.it>
-
-This node implements an user interface ,which helps user to access different parts via shell terminal
-
-:Master node description:
-       gets user request to choose robot behaviour
-       
-       using robot_state rosparam
-       following part:
-       
-       1. movebase client
-       
-       2. teleop keyboard
-       
-       3. assisted teleop
-"""
 import rospy
-from std_srvs.srv import *
-import os
+import actionlib
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+import time
+from std_msgs.msg import String
+
+"""
+    movebase client Node:
+        gets desired position from user and sends it to movebase node using 
+        actionlib and if the goal is not reached before timeout cancels it
+"""
+
+goal_x = -5.0
+goal_y = 5.0
+
+def callback_movebase_client_goal(data):
+    global goal_x
+    global goal_y
+    goal = data.data
+    goal_list = goal.split(',')
+    goal_x = goal_list[0]
+    goal_y = goal_list[1]
 
 
-def change_state():
-
-    """
-.. function:: Change_state function for changes robot's behaviour and sends the corresponding response to other three nodes, use param to change state of robot
-
-    param userdata is the structure containing the data shared among states
-
-    :param 1: move client
-    :param 2: teleop keybord
-    :param 3: assisted teleop
-    :param limit: maximum number of stack frames to show
-    :type limit: integer number
-    :rtype: list of strings
-    """
-    os.system('cls||clear')
-    print("** MASTER NODE **\n")
-
-    # gets robot behaviour from user
-    x = input('''Choose robot behaviour:
-    1. reach point(x,y) autonomously
-    2. drive with keyboard
-    3. drive robot with collision avoidance\n
-    input: ''')
-
-    if x == '1':
-        rospy.set_param('robot_state', '1')
-        print("\nstate changed: movebase client")
-    elif x == '2':
-        rospy.set_param('robot_state', '2')
-        print("\nstate changed: teleop keyboard")
-    elif x == '3':
-        rospy.set_param('robot_state', '3')
-        print("\nstate changed: assisted teleop")
+def movebase_clinet():
+    global goal_x
+    global goal_y
+    # Create an action client called "move_base" with action definition file "MoveBaseAction"
+    client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
+    # Creates a new goal with the MoveBaseGoal constructor
+    goal = MoveBaseGoal()
+    goal.target_pose.header.frame_id = "map"
+    goal.target_pose.header.stamp = rospy.Time.now()
+    # Gets goal position from user
+    goal.target_pose.pose.position.x = float(goal_x)
+    goal.target_pose.pose.position.y = float(goal_y)
+    # No rotation of the mobile base frame w.r.t. map frame
+    goal.target_pose.pose.orientation.w = 1.0
+    # Waits until the action server has started up and started listening for goals.
+    client.wait_for_server()
+    # Sends the goal to the action server.
+    client.send_goal(goal)
+    print("waiting for robot to reach the target wihthin 20 seconds")
+    finished_before_timeout = client.wait_for_result(timeout=rospy.Duration(20))
+    # detects if the target is reached before timeout
+    if finished_before_timeout:
+        print("Target reached!")
+        time.sleep(3)
+        return client.get_result()
+    else:
+        print("Action did not finish before time out!")
+        time.sleep(3)
+        client.cancel_all_goals()
 
 
 def main():
-    """
-.. function:: main function, creat initiation node for master script
-    set param zero like first state of robot (User interface) 
-    Start the timer with 20 Hz.
-    """
-
-    rospy.init_node('master')
+    rospy.init_node('movebase_client')
+    rospy.Subscriber("movebase_client_goal", String, callback_movebase_client_goal)
     rospy.set_param('robot_state', '0')
     rate = rospy.Rate(20)
     while not rospy.is_shutdown():
-        if rospy.get_param('robot_state') == '0':
-            change_state()
+        if rospy.get_param('robot_state')=='1':
+            movebase_clinet()
         else:
             rate.sleep()
             continue
         rate.sleep()
 
-
-# If the python node is executed as main process (sourced directly)
 if __name__ == '__main__':
     main()
